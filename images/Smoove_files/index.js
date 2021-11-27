80a9c34dc806da06a79a19dd028ba3f2d971ec83
s0wie <sowieh@gmail.com>
2021-11-27 16:54:29 +0100

    Tried adding a footer and store

--- images/Smoove_files/index.js
+++
@@ -1,8 +0,0 @@
-
-// nav bg on scroll
-
-window.onscroll = () => {
-  const nav = document.querySelector('nav');
-    if(this.scrollY <= 40) nav.className = '';
-    else nav.className = 'scroll';
-};
