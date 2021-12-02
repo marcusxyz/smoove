71415a79c1be6dd84e7b3e7fc0fb2da1805fb5a4
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2021-12-02 14:56:33 +0100

    cleaned up js file

--- js/index.js
+++ js/index.js
@@ -61,31 +61,30 @@ window.addEventListener('scroll', function () {
   }
 });
 
-// Vanilla JavaScript Scroll to Anchor
-// @ https://perishablepress.com/vanilla-javascript-scroll-anchor/
+// Scroll to Anchor
 
-(function () {
-  scrollTo();
-})();
-
-function scrollTo() {
+const scrollTo = () => {
   const links = document.querySelectorAll('.scroll');
   links.forEach((each) => (each.onclick = scrollAnchors));
 }
 
-function scrollAnchors(e, respond = null) {
+const scrollAnchors = (e, respond = null) => {
   const distanceToTop = (el) => Math.floor(el.getBoundingClientRect().top);
   e.preventDefault();
-  var targetID = respond
+
+  const targetID = respond
     ? respond.getAttribute('href')
     : this.getAttribute('href');
+
   const targetAnchor = document.querySelector(targetID);
   if (!targetAnchor) return;
+
   const originalTop = distanceToTop(targetAnchor);
   window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+
   const checkIfDone = setInterval(function () {
-    const atBottom =
-      window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+    const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+
     if (distanceToTop(targetAnchor) === 0 || atBottom) {
       targetAnchor.tabIndex = '-1';
       targetAnchor.focus();
@@ -93,4 +92,5 @@ function scrollAnchors(e, respond = null) {
       clearInterval(checkIfDone);
     }
   }, 100);
+
 }
